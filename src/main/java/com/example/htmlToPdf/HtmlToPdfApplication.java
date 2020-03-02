03ce4c6ddbbc871959187cdba749d3fd2075ccec
Kiran Dhongade <kinnsdhongadi@gmail.com>
2020-03-02 10:21:45 +0530

    Update HtmlToPdfApplication.java
    Refactor

--- src/main/java/com/example/htmlToPdf/HtmlToPdfApplication.java
+++ src/main/java/com/example/htmlToPdf/HtmlToPdfApplication.java
@@ -81,7 +81,7 @@ public class HtmlToPdfApplication {
 
 			document = new Document();
 			// document header attributes
-			document.addAuthor("Kiran Dhongade");
+			document.addAuthor("Kinns123");
 			document.addCreationDate();
 			document.addProducer();
 			document.addCreator("kinns123.github.io");
